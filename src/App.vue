4a1b1cc9fad94c1116a068780fc6ba4ffa7d00d1
hossein <hossein.bajan@gmail.com>
2025-12-04 23:00:18 +0330

    refactor: update App.vue layout and integrate Tailwind CSS with DaisyUI
    - Replaced the existing content in App.vue with a button.
    - Updated main.ts to import Tailwind CSS styles.
    - Added a new style.css file to include Tailwind CSS and DaisyUI.
    - Modified vite.config.ts to include the Tailwind CSS plugin.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <button class="btn btn-soft">
+      Default
+    </button>
+    <button class="btn btn-soft btn-primary">
+      Primary
+    </button>
+    <button class="btn btn-soft btn-secondary">
+      Secondary
+    </button>
+    <button class="btn btn-soft btn-accent">
+      Accent
+    </button>
+    <button class="btn btn-soft btn-info">
+      Info
+    </button>
+    <button class="btn btn-soft btn-success">
+      Success
+    </button>
+    <button class="btn btn-soft btn-warning">
+      Warning
+    </button>
+    <button class="btn btn-soft btn-error">
+      Error
+    </button>
+  </div>
 </template>
-
-<style scoped></style>
