8136f7e13b0e5ea64ec130e8eba1a39dc004b943
hossein <hossein.bajan@gmail.com>
2025-12-04 22:32:58 +0330

    feat: install and config husky, lint-stage and commitlint for statndard commits

---
+++ commitlint.config.ts
@@ -0,0 +1,20 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat',
+      'fix',
+      'docs',
+      'style',
+      'refactor',
+      'perf',
+      'test',
+      'chore',
+      'build',
+      'ci',
+      'revert',
+    ]],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+  },
+}
