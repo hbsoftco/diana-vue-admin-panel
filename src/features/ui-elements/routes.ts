3788367eea6c4f899bfc1ca6e5f4bbb0a260bef0
hossein <hossein.bajan@gmail.com>
2025-12-13 22:20:43 +0330

    feat: update UI elements with new routes, enhance layout, and improve localization support

--- src/features/ui-elements/routes.ts
+++ src/features/ui-elements/routes.ts
@@ -11,78 +11,211 @@ export const uiElementRoutes = {
     {
       path: 'alerts',
       component: () => import('@/pages/ui-elements/alerts.vue'),
+      meta: {
+        pageTitle: 'pages.alerts.alerts',
+        breadcrumb: [
+          { label: 'pages.alerts.title', link: '/ui-elements' },
+          { label: 'pages.alerts.alerts' },
+        ],
+      },
     },
     {
       path: 'badge',
       component: () => import('@/pages/ui-elements/badge.vue'),
+      meta: {
+        pageTitle: 'pages.badge.badge',
+        breadcrumb: [
+          { label: 'pages.badge.title', link: '/ui-elements' },
+          { label: 'pages.badge.badge' },
+        ],
+      },
     },
     {
       path: 'breadcrumb',
       component: () => import('@/pages/ui-elements/breadcrumb.vue'),
+      meta: {
+        pageTitle: 'pages.breadcrumb.breadcrumb',
+        breadcrumb: [
+          { label: 'pages.breadcrumb.title', link: '/ui-elements' },
+          { label: 'pages.breadcrumb.breadcrumb' },
+        ],
+      },
     },
     {
       path: 'buttons',
       component: () => import('@/pages/ui-elements/buttons.vue'),
+      meta: {
+        pageTitle: 'pages.buttons.buttons',
+        breadcrumb: [
+          { label: 'pages.buttons.title', link: '/ui-elements' },
+          { label: 'pages.buttons.buttons' },
+        ],
+      },
     },
     {
       path: 'button-group',
       component: () => import('@/pages/ui-elements/button-group.vue'),
+      meta: {
+        pageTitle: 'pages.buttonGroup.buttonGroup',
+        breadcrumb: [
+          { label: 'pages.buttonGroup.title', link: '/ui-elements' },
+          { label: 'pages.buttonGroup.buttonGroup' },
+        ],
+      },
     },
     {
       path: 'cards',
       component: () => import('@/pages/ui-elements/cards.vue'),
+      meta: {
+        pageTitle: 'pages.card.cards',
+        breadcrumb: [
+          { label: 'pages.card.title', link: '/ui-elements' },
+          { label: 'pages.card.cards' },
+        ],
+      },
     },
     {
       path: 'dropdowns',
       component: () => import('@/pages/ui-elements/dropdowns.vue'),
+      meta: {
+        pageTitle: 'pages.dropdowns.dropdowns',
+        breadcrumb: [
+          { label: 'pages.dropdowns.title', link: '/ui-elements' },
+          { label: 'pages.dropdowns.dropdowns' },
+        ],
+      },
     },
     {
       path: 'images-figures',
       component: () => import('@/pages/ui-elements/images-figures.vue'),
+      meta: {
+        pageTitle: 'pages.imagesFigures.imagesFigures',
+        breadcrumb: [
+          { label: 'pages.imagesFigures.title', link: '/ui-elements' },
+          { label: 'pages.imagesFigures.imagesFigures' },
+        ],
+      },
     },
     {
       path: 'links-interactions',
       component: () => import('@/pages/ui-elements/links-interactions.vue'),
+      meta: {
+        pageTitle: 'pages.linksInteractions.linksInteractions',
+        breadcrumb: [
+          { label: 'pages.linksInteractions.title', link: '/ui-elements' },
+          { label: 'pages.linksInteractions.linksInteractions' },
+        ],
+      },
     },
     {
       path: 'list-group',
       component: () => import('@/pages/ui-elements/list-group.vue'),
+      meta: {
+        pageTitle: 'pages.listGroup.listGroup',
+        breadcrumb: [
+          { label: 'pages.listGroup.title', link: '/ui-elements' },
+          { label: 'pages.listGroup.listGroup' },
+        ],
+      },
     },
     {
       path: 'navs-tabs',
       component: () => import('@/pages/ui-elements/navs-tabs.vue'),
+      meta: {
+        pageTitle: 'pages.navsTabs.navsTabs',
+        breadcrumb: [
+          { label: 'pages.navsTabs.title', link: '/ui-elements' },
+          { label: 'pages.navsTabs.navsTabs' },
+        ],
+      },
     },
     {
       path: 'object-fit',
       component: () => import('@/pages/ui-elements/object-fit.vue'),
+      meta: {
+        pageTitle: 'pages.objectFit.objectFit',
+        breadcrumb: [
+          { label: 'pages.objectFit.title', link: '/ui-elements' },
+          { label: 'pages.objectFit.objectFit' },
+        ],
+      },
     },
     {
       path: 'pagination',
       component: () => import('@/pages/ui-elements/pagination.vue'),
+      meta: {
+        pageTitle: 'pages.pagination.pagination',
+        breadcrumb: [
+          { label: 'pages.pagination.title', link: '/ui-elements' },
+          { label: 'pages.pagination.pagination' },
+        ],
+      },
     },
     {
       path: 'popovers',
       component: () => import('@/pages/ui-elements/popovers.vue'),
+      meta: {
+        pageTitle: 'pages.popovers.popovers',
+        breadcrumb: [
+          { label: 'pages.popovers.title', link: '/ui-elements' },
+          { label: 'pages.popovers.popovers' },
+        ],
+      },
     },
     {
       path: 'progress',
       component: () => import('@/pages/ui-elements/progress.vue'),
+      meta: {
+        pageTitle: 'pages.progress.progress',
+        breadcrumb: [
+          { label: 'pages.progress.title', link: '/ui-elements' },
+          { label: 'pages.progress.progress' },
+        ],
+      },
     },
     {
       path: 'spinners',
       component: () => import('@/pages/ui-elements/spinners.vue'),
+      meta: {
+        pageTitle: 'pages.spinners.spinners',
+        breadcrumb: [
+          { label: 'pages.spinners.title', link: '/ui-elements' },
+          { label: 'pages.spinners.spinners' },
+        ],
+      },
     },
     {
       path: 'toasts',
       component: () => import('@/pages/ui-elements/toasts.vue'),
+      meta: {
+        pageTitle: 'pages.toasts.toasts',
+        breadcrumb: [
+          { label: 'pages.toasts.title', link: '/ui-elements' },
+          { label: 'pages.toasts.toasts' },
+        ],
+      },
     },
     {
       path: 'tooltips',
       component: () => import('@/pages/ui-elements/tooltips.vue'),
+      meta: {
+        pageTitle: 'pages.tooltips.tooltips',
+        breadcrumb: [
+          { label: 'pages.tooltips.title', link: '/ui-elements' },
+          { label: 'pages.tooltips.tooltips' },
+        ],
+      },
     },
     {
       path: 'typography',
       component: () => import('@/pages/ui-elements/typography.vue'),
+      meta: {
+        pageTitle: 'pages.typography.typography',
+        breadcrumb: [
+          { label: 'pages.typography.title', link: '/ui-elements' },
+          { label: 'pages.typography.typography' },
+        ],
+      },
     },
   ],
 }
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
 import AppHeader from '@shared/ui/layout/AppHeader.vue'
 import AppSidebar from '@shared/ui/layout/AppSidebar.vue'
+import { watchEffect } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const { t } = useI18n()
+
+// Set document title from route meta
+watchEffect(() => {
+  const pageTitle = (route.meta.pageTitle as string) || 'menu.dashboards'
+  document.title = t(pageTitle)
+})
 </script>
 
 <template>
@@ -10,7 +22,30 @@ import AppSidebar from '@shared/ui/layout/AppSidebar.vue'
     <div class="flex-1 flex flex-col">
       <AppHeader />
 
-      <main class="flex-1 overflow-y-auto p-4">
+      <main class="flex-1 overflow-y-auto px-4">
+        <div v-if="route?.meta" class="flex block items-center justify-between my-4">
+          <h1 class="font-semibold text-base-content text-lg">
+            {{ $t((route?.meta?.pageTitle as string) || 'menu.dashboards') }}
+          </h1>
+          <div class="breadcrumbs text-xs">
+            <ul>
+              <li
+                v-for="(crumb, index) in (route.meta.breadcrumb as {
+                  label: string
+                  link?: string
+                }[]) || []"
+                :key="index"
+                class="font-medium"
+              >
+                <RouterLink v-if="crumb.link" :to="crumb.link">
+                  {{ $t(crumb.label) }}
+                </RouterLink>
+                <span v-else>{{ $t(crumb.label) }}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
         <RouterView />
       </main>
     </div>
